40ead26c9613fa5490621be46043808bdef15085
Martin Buberl <mail@martinbuberl.com>
2012-02-11 14:29:40 -0500

    Add some test code

--- src/MSBuild.Version.Tasks/GitVersionFile.cs
+++ src/MSBuild.Version.Tasks/GitVersionFile.cs
@@ -1,11 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using MSBuild.Version.Tasks.Exceptions;
 
 namespace MSBuild.Version.Tasks
 {
-    class GitVersionFile
+    /// <remarks>
+    /// This is a heavily modified fork of the corresponding task in the MSBuild Versioning project <see href="http://versioning.codeplex.com/" />.
+    /// This was necessary because we use TeamCity as our build server. TeamCity checks out the source code without the '.git' folder, which would
+    /// lead to an exception. Therefore a different exception handling implementation was necessary.
+    /// </remarks>
+    public class GitVersionFile : VersionInfoBase
     {
+        internal override void SetVersionInfo()
+        {
+            try
+            {   
+                // InitRevision
+                var bla = ExecuteCommand("git.exe", "rev-list");
+                // IsWorkingCopyDirty
+                var foo = ExecuteCommand("git.exe", "diff-index --quiet HEAD");
+                // GetBranch
+                var branch = ExecuteCommand("git.exe", "describe --all")[0];
+                // GetTags
+                var tags = ExecuteCommand("git.exe", "describe")[0];
+            }
+            catch (ExecuteCommandException ex)
+            {
+                
+                throw;
+            }
+        }
     }
 }
