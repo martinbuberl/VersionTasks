e32d05115335c8d016851dc423d8e79516d4b1d2
Martin Buberl <mail@martinbuberl.com>
2012-03-15 20:18:23 -0400

    Fixed the TearDown System.UnauthorizedAccessException exception; Add tests to check repository for uncommitted changes

--- src/MSBuild.Version.Tasks.Tests/GitVersionFileTest.cs
+++ src/MSBuild.Version.Tasks.Tests/GitVersionFileTest.cs
@@ -24,21 +24,39 @@ namespace MSBuild.Version.Tasks.Tests
         [TearDown]
         public void TearDown()
         {
+            // in combination with TortoiseGit "Directory.Delete()' throws an "System.UnauthorizedAccessException'
+            VersionInfoBase versionInfoBase = new GitVersionFile();
+            versionInfoBase.WorkingDirectory = TempDirectory;
+            versionInfoBase.ExecuteCommand("rm", "-rf *");
+
             // delete the 'temp' folder
             Directory.Delete(TempDirectory, true);
         }
 
         [Test]
-        public void Execute_WriteRevision_ShouldWriteRevision()
+        public void Execute_WriteChangeset_ShouldWriteChangeset()
         {
-            Execute("Revision.tmp", "Git1Revision.txt", "git1");
-            Assert.AreEqual(ReadFirstLine("Git1Revision.txt"), "2");
+            Execute("Changeset.tmp", "Git1Changeset.txt", "Git1");
+            Assert.AreEqual("ca2e4be0861b6247930a1f20b2d40ae84ac12904", ReadFirstLine("Git1Changeset.txt"));
 
-            Execute("Revision.tmp", "Git2Revision.txt", "git2");
-            Assert.AreEqual(ReadFirstLine("Git2Revision.txt"), "4");
+            Execute("Changeset.tmp", "Git2Changeset.txt", "Git2");
+            Assert.AreEqual("2b7c08e1784ca823ae09fef7fcafd5bdc4bff3f2", ReadFirstLine("Git2Changeset.txt"));
 
-            Execute("Revision.tmp", "Git3Revision.txt", "git3");
-            Assert.AreEqual(ReadFirstLine("Git3Revision.txt"), "2");
+            Execute("Changeset.tmp", "Git3Changeset.txt", "Git3");
+            Assert.AreEqual("fbfc1dc257275d928b779d9c3a073ba00b0d9701", ReadFirstLine("Git3Changeset.txt"));
+        }
+
+        [Test]
+        public void Execute_WriteDirty_ShouldWriteDirty()
+        {
+            Execute("Dirty.tmp", "Git1Dirty.txt", "Git1");
+            Assert.AreEqual("0", ReadFirstLine("Git1Dirty.txt"));
+
+            Execute("Dirty.tmp", "Git2Dirty.txt", "Git2");
+            Assert.AreEqual("1", ReadFirstLine("Git2Dirty.txt"));
+
+            Execute("Dirty.tmp", "Git3Dirty.txt", "Git3");
+            Assert.AreEqual("0", ReadFirstLine("Git3Dirty.txt"));
         }
 
         private static readonly string TemplatesDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data\\Templates");
