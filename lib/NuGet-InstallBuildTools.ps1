74a137b1eba96ca9b7210332258579111cf75b3a
Martin Buberl <mail@martinbuberl.com>
2012-08-26 18:26:44 -0400

    Removed ps script to install tools packages; Add new tools package.config

--- lib/NuGet-InstallBuildTools.ps1
+++
@@ -1,7 +0,0 @@
-$currentPath = Split-Path $MyInvocation.MyCommand.Path
-
-& "$currentPath\nuget" install NUnit.Runners
-& "$currentPath\nuget" install MSBuildTasks
-
-Write-Host "Press any key to continue . . ."
-$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
