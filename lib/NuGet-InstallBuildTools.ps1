d800a0c35aeddb6e98bc2452333f3fb3def22571
Martin Buberl <mail@martinbuberl.com>
2012-08-26 14:15:34 -0400

    Add MSBuild.Community.Tasks package

--- lib/NuGet-InstallBuildTools.ps1
+++ lib/NuGet-InstallBuildTools.ps1
@@ -1,6 +1,7 @@
 $currentPath = Split-Path $MyInvocation.MyCommand.Path
 
-& "$currentPath\nuget" install NUnit.Runners -Version 2.6.1
+& "$currentPath\nuget" install NUnit.Runners
+& "$currentPath\nuget" install MSBuildTasks
 
 Write-Host "Press any key to continue . . ."
 $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
